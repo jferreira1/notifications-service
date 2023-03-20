361f44b5381c35d722fe533caaa430afe420f648
Jean Ferreira <jeansouzza@gmail.com>
2023-03-20 17:29:35 -0300

    remove unused imports

--- src/app/useCases/count-recipient-notifications.ts
+++ src/app/useCases/count-recipient-notifications.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Notification } from '../entities/notification/notification';
 import { NotificationsRepository } from '../repositories/notifications-repository';
-import { NotificationNotFound } from './errors/notification-not-found';
 
 interface CountRecipientNotificationsRequest {
   recipientId: string;
